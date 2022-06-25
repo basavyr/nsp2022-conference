9aa964c953343b6e81177df5db24918027bfa6d8
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-06-25 14:02:21 +0300

    add fit in a self-contained object

--- Code/python/Fitting_136Nd.py
+++ Code/python/Fitting_136Nd.py
@@ -19,6 +19,7 @@ def chi2(exp_data, th_data):
         sum_0 += e/exp_data[i]
     chi = 1.0/len(exp_data+1)*sum_0
     print(np.sqrt(chi))
+    return chi
 
 
 ############################################################################
@@ -32,25 +33,26 @@ band_heads_136Nd_higher = np.asarray(
     [Nd136.Band_Head_Spin_Higher for _ in spins_136Nd_higher])
 x_data_136Nd_higher = (
     spins_136Nd_higher, phonons_136Nd_higher, band_heads_136Nd_higher)
-
 energies_exp_136Nd_higher = np.concatenate(
     (Nd136.Energy_Band1_Higher, Nd136.Energy_Band2_Higher, Nd136.Energy_Band3_Higher))
 
 
 guess136Nd_higher = [1.0/(2.0*10.0), 1.0/(2.0*25.0), 1.0/(2.0*1.0)]
 
-fit_parameters_136Nd_higher, _ = curve_fit(
-    Models.Model_Energy, x_data_136Nd_higher, energies_exp_136Nd_higher, p0=guess136Nd_higher, bounds=BOUNDS)
 
-# evaluate the chi_2 function
-e1_higher = fit_parameters_136Nd_higher[0]
-e2_higher = fit_parameters_136Nd_higher[1]
-e3_higher = fit_parameters_136Nd_higher[2]
-print(1/(2.0*e1_higher))
-print(1/(2.0*e2_higher))
-print(1/(2.0*e3_higher))
-chi2(energies_exp_136Nd_higher, Models.Model_Energy(
-    x_data_136Nd_higher, e1_higher, e2_higher, e3_higher))
+def do_fit_higher(guess):
+    fit_parameters_136Nd_higher, _ = curve_fit(
+        Models.Model_Energy, x_data_136Nd_higher, energies_exp_136Nd_higher, p0=guess136Nd_higher, bounds=BOUNDS)
+
+    # evaluate the chi_2 function
+    e1_higher = fit_parameters_136Nd_higher[0]
+    e2_higher = fit_parameters_136Nd_higher[1]
+    e3_higher = fit_parameters_136Nd_higher[2]
+    print(1/(2.0*e1_higher))
+    print(1/(2.0*e2_higher))
+    print(1/(2.0*e3_higher))
+    chi2(energies_exp_136Nd_higher, Models.Model_Energy(
+        x_data_136Nd_higher, e1_higher, e2_higher, e3_higher))
 ############################################################################
 ############################################################################
 ############################################################################
@@ -59,3 +61,32 @@ chi2(energies_exp_136Nd_higher, Models.Model_Energy(
 ############################################################################
 # fit for 136Nd lower  ####################################################
 ############################################################################
+spins_136Nd_lower = np.concatenate(
+    (Nd136.Spins_Band1_Lower, Nd136.Spins_Band2_Lower))
+phonons_136Nd_lower = np.concatenate(
+    (Nd136.Phonons_Band1_Lower, Nd136.Phonons_Band2_Lower))
+band_heads_136Nd_lower = np.asarray(
+    [Nd136.Band_Head_Spin_Lower for _ in spins_136Nd_lower])
+x_data_136Nd_lower = (
+    spins_136Nd_lower, phonons_136Nd_lower, band_heads_136Nd_lower)
+energies_exp_136Nd_lower = np.concatenate(
+    (Nd136.Energy_Band1_Lower, Nd136.Energy_Band2_Lower))
+
+
+guess136Nd_lower = [1.0/(2.0*10.0), 1.0/(2.0*25.0), 1.0/(2.0*1.0)]
+
+
+def do_fit_lower(guess):
+    fit_parameters_136Nd_lower, _ = curve_fit(
+        Models.Model_Energy, x_data_136Nd_lower, energies_exp_136Nd_lower, p0=guess, bounds=BOUNDS)
+
+    # evaluate the chi_2 function
+    e1_lower = fit_parameters_136Nd_lower[0]
+    e2_lower = fit_parameters_136Nd_lower[1]
+    e3_lower = fit_parameters_136Nd_lower[2]
+    print(1/(2.0*e1_lower))
+    print(1/(2.0*e2_lower))
+    print(1/(2.0*e3_lower))
+    x = chi2(energies_exp_136Nd_lower, Models.Model_Energy(
+        x_data_136Nd_lower, e1_lower, e2_lower, e3_lower))
+    return x
--- Code/python/Nd136.py
+++ Code/python/Nd136.py
@@ -16,20 +16,12 @@ def MeV(energy):
 # both bands have the band-head (first spin state from the ground-band) as 10^+
 # Structure 1 has a lower energy for the 10^+ state
 
+# data for Structure 1
 band3_spins = np.arange(10, 18, 2)
 band3_energy_exp = sorted([5841, 4847, 3995, 3277])
 band4_spins = np.arange(11, 17, 2)
 band4_energy_exp = sorted([5346, 4545, 4026])
 
-
-band2_spins = np.arange(10, 24, 2)
-band2_energy_exp = sorted([8615, 7349, 6188, 5189, 4345, 3684, 3294])
-band5_spins = np.arange(13, 23, 2)
-band5_energy_exp = sorted([8098, 6928, 5940, 5130, 4452])
-band6_spins = np.arange(14, 26, 2)
-band6_energy_exp = sorted([9171, 8222, 7329, 6470, 5568, 4853])
-
-# data for Structure 1
 Spins_Band1_Lower = band3_spins[1:]
 Spins_Band2_Lower = band4_spins
 Band_Head_Spin_Lower = band3_spins[0]
@@ -43,6 +35,13 @@ Phonons_Band2_Lower = [1 for _ in Energy_Band2_Lower]
 
 
 # data for Structure 2
+band2_spins = np.arange(10, 24, 2)
+band2_energy_exp = sorted([8615, 7349, 6188, 5189, 4345, 3684, 3294])
+band5_spins = np.arange(13, 23, 2)
+band5_energy_exp = sorted([8098, 6928, 5940, 5130, 4452])
+band6_spins = np.arange(14, 26, 2)
+band6_energy_exp = sorted([9171, 8222, 7329, 6470, 5568, 4853])
+
 Spins_Band1_Higher = band2_spins[1:]
 Spins_Band2_Higher = band5_spins
 Spins_Band3_Higher = band6_spins
