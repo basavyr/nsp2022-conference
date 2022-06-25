12d01c878273803ade8f047dac32131a9619816b
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-06-25 11:58:39 +0300

    add fit parameters for isomer state

--- Code/python/Energies.py
+++ Code/python/Energies.py
@@ -31,8 +31,8 @@ def Wobbling_Frequency(spin, A1, A2, A3):
     t1_2 = np.power(t1, 2)
     t2_2 = np.power(t2, 2)
 
-    if(t1_2 <= t2_2):
-        return MAX_VAL
+    # if(t1_2 <= t2_2):
+    #     return MAX_VAL
 
     h_omega_wob = np.sqrt(t1_2-t2_2)
 
@@ -47,8 +47,8 @@ def Absolute_Energy(spin, phonon_number, A1, A2, A3):
     n = phonon_number
 
     h_omega_wob = Wobbling_Frequency(I, A1, A2, A3)
-    if(h_omega_wob == MAX_VAL):
-        return MAX_VAL
+    # if(h_omega_wob == MAX_VAL):
+    #     return MAX_VAL
 
     rotation_term = A3*I*(I+1.0)
     harmonic_term = h_omega_wob*(n+0.5)
@@ -58,33 +58,31 @@ def Absolute_Energy(spin, phonon_number, A1, A2, A3):
     return energy
 
 
-def Excitation_Energy(spin, phonon_number, band_head_absolute_energy, A1, A2, A3):
+def Excitation_Energy(spin, phonon_number, A1, A2, A3):
     """
     Calculate the excitation energy for a given state 
     The excitation energy is evaluated as the difference between the absolute energy of that state and the band-head energy
     The function should have band-head spin as an argument
     """
 
-    # I_0 = band_head_spin
-
-    # # first evaluate the energy of the band head
-    # E_0 = Absolute_Energy(I_0, 0, A1, A2, A3)
-    # if(E_0 == MAX_VAL):
-    #     return MAX_VAL
+    # first evaluate the energy of the band head
+    # put by hand for the 10^+ level in 134Ce
+    E_0 = Absolute_Energy(10, 0, A1, A2, A3)
+    if(E_0 == MAX_VAL):
+        return MAX_VAL
 
     I = spin
     n = phonon_number
-    E_0 = band_head_absolute_energy
 
     # evaluate the absolute energy of the I level state
     E_I = Absolute_Energy(I, n, A1, A2, A3)
-    if(E_I == MAX_VAL):
-        return MAX_VAL
+    # if(E_I == MAX_VAL):
+    #     return MAX_VAL
 
     return E_I-E_0
 
 
-def Model_Energy(x_data, band_head_absolute_energy, A1, A2, A3):
+def Model_Energy(x_data, A1, A2, A3):
     """
     the model function that will be used in the curve_fit  
     the function must be capable of dealing with arrays for the spin and the phonon number
@@ -93,7 +91,6 @@ def Model_Energy(x_data, band_head_absolute_energy, A1, A2, A3):
     # unpack the x-data in terms of the spins and the phonon numbers
     spins, phonons = x_data
 
-    E_0 = band_head_absolute_energy
-    local_energies = Excitation_Energy(spins, phonons, E_0, A1, A2, A3)
+    local_energies = Excitation_Energy(spins, phonons, A1, A2, A3)
 
     return local_energies
--- Code/python/Fitting.py
+++ Code/python/Fitting.py
@@ -1,11 +1,28 @@
+from ntpath import join
 import numpy as np
 from scipy.optimize import curve_fit
 
 
 import Energies as Models
-
+import Ce134
 
 # add limits for the inertia factors
+# these limits correspond to 1<=MOI<=125 \hbar^2MeV^-1
 A_MIN = 0.004
 A_MAX = 1.0
-# these limits correspond to 1<=MOI<=125 \hbar^2MeV^-1
+BOUNDS = ([A_MIN,A_MIN,A_MIN], [A_MAX,A_MAX,A_MAX])
+
+
+############################################################################
+# fit for 134Ce ############################################################
+############################################################################
+spins_134Ce_isomer=np.concatenate((Ce134.Spins_Band1_Isomer,Ce134.Spins_Band2_Isomer))
+phonons_134Ce_isomer=np.asarray(Ce134.Phonons_Band1_Isomer+Ce134.Phonons_Band2_Isomer)
+x_data_134Ce_isomer=(spins_134Ce_isomer,phonons_134Ce_isomer)
+experimental_energies_134Ce_isomer=np.asarray(Ce134.Energy_Band1_Isomer+Ce134.Energy_Band2_Isomer)
+guess134Ce_isomer=[0.8,0.5,0.1]
+fit_parameters_134Ce_isomer, _=curve_fit(Models.Model_Energy,x_data_134Ce_isomer,experimental_energies_134Ce_isomer,p0=guess134Ce_isomer,bounds=BOUNDS)
+print(fit_parameters_134Ce_isomer)
+############################################################################
+############################################################################
+############################################################################
